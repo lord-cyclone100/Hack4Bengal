f89910d2452e6118970ce5967724007204895a35
lord-cyclone100 <sang66deb@gmail.com>
2025-06-18 21:24:35 +0530

    Made card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Navbar } from './components/Navbar'
 import { Carousel } from './components/Carousel'
+import { Card } from './components/Card'
 
 export const App = () => {
   return (
@@ -9,6 +10,8 @@ export const App = () => {
       <div className='flex flex-col gap-8'>
         <Navbar />
         <Carousel />
+        <h1 className='font-game2'>Hello World</h1>
+        <Card />
       </div>
     </>
   )
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,22 +1,26 @@
 export const Card = () =>{
     return(
-        <div className="card bg-base-100 w-96 shadow-sm">
-          <figure>
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">
-              Card Title
-              <div className="badge badge-secondary">Action</div>
-            </h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">Fashion</div>
-              <div className="badge badge-outline">Products</div>
+      <div className="w-80 bg-gradient-to-br from-white/5 via-white/10 to-white/20 backdrop-blur-md border border-white/30 rounded-lg shadow-lg">
+        <figure className="px-2 pt-2">
+          <img src="https://wallpapers.com/images/hd/fortnite-background-34h5tua0czqu7u9t.jpg"className="rounded-xl" />
+        </figure>
+        <div className="card-body flex gap-4 font-game">
+          <div className="flex justify-between items-center">
+            <p>26 December 2025</p>
+            <p className="font-game2 tracking-wide">Fortnight</p>
+          </div>
+          <h1 className="card-title font-extrabold">Winter 2025 Tourney</h1>
+          <p>One more contest is coming up upon us. Make sure to participate.</p>
+          <div class="relative inline-block w-full font-game2 tracking-wide">
+            <button class="w-full h-10 bg-blue-600 text-white rounded-lg pr-4 text-right">Participate Now</button>
+            <div class="absolute top-0 left-0 w-1/2 h-10 bg-amber-300 text-black font-semibold rounded-lg rounded-br-[20vw] rounded-tr-none pointer-events-none flex items-center justify-items-start pl-4">
+              Prize Pool :- 500$
             </div>
           </div>
+          <div className="flex justify-end">
+            <h3>Host Name</h3>
+          </div>
         </div>
+      </div>
     )
 }
